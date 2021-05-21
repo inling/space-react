7a800133ce8ea97aaad8c10664db46acd44209f4
芮杰 <ruijie@ruijiedeMacBook-Pro.local>
2021-05-20 23:57:02 -0700

    JSX其实也是一个对象

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 /**
- * 2. JSX属性和表达式
- * html backgroundColor:'red'
- * class => className
- * for => htmlFor
- * style =>对象
- * 如果想在JSX中读取JS变量的话，可以用{}包起来
+ * 3.JSX其实也是一个对象
+ * 可以把jsx赋值给变量，还可以作为方法的参数，作为方法的返回值。
  */
-//let element1 = <h1 id="title">hello</h1>;
-let element1 = <h1 id="title" htmlFor="title" className="title" style={{ color: 'red', backgroundColor:'green' }}>hello <span></span> world</h1>;
-console.log(element1)
-//jsx编译成createElement是在webpack编译的时候,也就是打包的时候执行的
-//打包后的代码在浏览器里执行的时候，会执行函数，返回一个JS对象
-let element2 = React.createElement("h1", {
-    id: "title"
-}, 'hello', 'world')
-console.log(element2)
-/**
- * {
- *  type:'h1',
- *  props:{
- *    id:'title',
- *    children:['hello','world'] 如果只有一个儿子的话，children就是那个独生子，如果有多个儿子，就是一个数组
- *  }
- * }
- */
-//render方法会负责吧虚拟DOM变成真实DOM插入容器里
-ReactDOM.render(element1, document.getElementById('root'));
+function greeting(name){
+    if(name){
+        return <h1>hello,{name}</h1>
+    }else{
+        return <h1>hello, Stranger</h1>
+    }
+}
+
+const element = greeting('zhufeng');
+ReactDOM.render(element, document.getElementById('root'));
