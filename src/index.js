28bfb6d20dfd614be5a758d768bb4e58f69cb531
芮杰 <ruijie@ruijiedeMacBook-Pro.local>
2021-05-21 00:43:03 -0700

    更新元素渲染

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 /**
- * 3.JSX其实也是一个对象
- * 可以把jsx赋值给变量，还可以作为方法的参数，作为方法的返回值。
+ * 4.6 更新元素渲染
+ * 4.7 React只会变更必要的部分
+ * React元素是不可变的
  */
-let users = [{ id: 1, name: '张三' }, { id: 2, name: '李四' }, { id: 3, name: '王五' }]
-let elements = users.map((user, index) => (<li key={index}>{user.name}</li>));
-ReactDOM.render(elements, document.getElementById('root'));
 
-setTimeout(()=>{
-    let users = [{ id: 2, name: '张三' }, { id: 3, name: '李四' }, { id: 1, name: '王五' }];
-    let elements = users.map((user, index) => (<li key={index}>{user.name}</li>));
-    ReactDOM.render(elements, document.getElementById('root'));
-},3000)
+function tick(){
+    const element = (
+        <div>
+            <div>当前时间</div>
+            {new Date().toLocaleTimeString()}
+            <div>中国</div>
+        </div>
+    )
+    ReactDOM.render(element,document.getElementById('root'))
+}
 
-/**
- * React 更新的时候会如何更新？
- * 1.简单粗暴 把删除的，再插入全部的新元素，性能比较差
- * 2.React会把老的虚拟Dom和新的DOM进行比较，这个也就是所谓的dom-diff
- * 找到它们之间的差异，然后通过打补丁的方式更新差异
- */
+setInterval(tick, 1000);
