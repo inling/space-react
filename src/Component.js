ae346f9279eff937dfecc2f4aa0053faf4f5af91
芮杰 <ruijie@ruijiedeMacBook-Pro.local>
2021-05-26 17:32:57 +0800

    6.状态的批量更新

--- src/Component.js
+++ src/Component.js
@@ -1,13 +1,21 @@
 import { createDOM } from './react-dom';
+class Updater{
+    constructor(classInstance){
+        this.classInstance = classInstance;//类组件的实例
+        this.pendingState = [];//等待生效的状态，可能是一个对象，也可能是一个函数
+    }
+}
 class Component {
     static isReactComponent = true;
     constructor(props) {
         this.props = props;
         this.state = {};
+        this.updater = new Updater(this);
     }
     setState(partialState) {
         let state = this.state;
         this.state = { ...state, ...partialState };
+        //此处调的是子类的render方法
         let newVdom = this.render();
         updateClassComponent(this, newVdom);
     }
@@ -16,6 +24,7 @@ class Component {
     }
 }
 
+//TODO更新类组件
 function updateClassComponent(classInstance, newVdom) {
     let oldDom = classInstance.dom; //取出这个类组件上次渲染出来的真实DOM
     let newDOM = createDOM(newVdom);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 /**
- * 类组件和类组件的更新
- * 可以在构造函数里，并且只能在构造函数中给this.state赋值
- * 定义状态对象
- * 属性对象 父组件给的，不能改变，是只读的
+ * 合成事件和批量更新
+ * 1.在React里，事件的更新可能是异步的，是批量的，不是同步的
+ *    调用state之后状态并没有立刻更新，而是先缓存起来了
+ *    等事件函数完成后，再进行批量更新，一次更新并重新渲染
+ * 因为jsx事件处理函数是react控制的，只要归react控制就是批量，只要不归react管了。就是非批量
  */
 
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name:this.props.name, number: 0 };
+    this.state = { name: this.props.name, number: 0 };
   }
   handleClick = () => {
-    this.setState({ number: this.state.number + 1 })
+    //肯定是批量更新，而且这个回调函数是等全部更新完成后才执行的
+    this.setState((lastState) => ({ number: lastState.number + 1 }), () => {
+      console.log('callback1', this.state.number);
+    })
+    console.log(this.state.number);
+    this.setState((lastState) => ({ number: lastState.number + 1 }), () => {
+      console.log('callback2', this.state.number);
+    })
+    console.log(this.state.number);
+
+    queueMicrotask(() => {
+      console.log(this.state.number);
+      this.setState((lastState) => ({ number: lastState.number + 1 }), () => {
+        console.log('callback3', this.state.number);
+      })
+      console.log(this.state.number);
+      this.setState((lastState) => ({ number: lastState.number + 1 }), () => {
+        console.log('callback4', this.state.number);
+      })
+      console.log(this.state.number);
+    });
+
+    setTimeout(() => {
+      console.log(this.state.number);
+      this.setState({ number: this.state.number + 1 })
+      console.log(this.state.number);
+      this.setState({ number: this.state.number + 1 })
+      console.log(this.state.number);
+    }, 1000);
   }
   render() {
     return (
